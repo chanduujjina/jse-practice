d462db3ed864bf5dc430b1f106e977622224bb29
INFYCHARMS <chandra.ujjina@macys.com>
2020-10-18 18:24:55 +0530

    prime number and right angle traingle program

--- README.md
+++ README.md
@@ -24,5 +24,77 @@ non access specifiers(abstract,final,strictft)
 
 8.detailed discussion on object and class
 
+//setter and getters
+//constructor
+
+
+1.when the instance varaible will load.
+
+while creating the object
+how many ways are there to set data to an onbject
+
+1.setter method
+
+2.constructor
+
+when we will go for constructor --> if the value is manadatory;
+when we will go for setter  --> if the value is optional
+
+
+control statements in java.
+
+1.if else
+2.switch block
+3.while
+4.do while
+5.for loop
+6.break
+7.continue
+6.java comments
+
+write a program to find given number is either odd or even
+
+write a program to give odd nmubers and even numbers in first 10 number using for loop
+
+write a program to give odd nmubers and even numbers in first 10 number using while loop
+
+1 to 5
+
+1 bad
+2 aveage
+
+3 good
+
+4.excellent
+
+write a program to verify whether given number is prime number or not
+
+write a program to find prime numbers between 1 to 20
+
+2,3,5,7,11,13,17,19
+
+*
+**
+***
+****
+*****
+
+
+*****
+****
+***
+*
+
+
+
+
+
+
+
+
+
+ 
+
+
 
 
--- src/com/demo/main/HandleCtrStatements.java
+++ src/com/demo/main/HandleCtrStatements.java
@@ -5,13 +5,19 @@ public class HandleCtrStatements {
 	public static void main(String[] args) {
 		
 		HandleCtrStatements ctrStatements = new  HandleCtrStatements();
-		String result=ctrStatements.verifyEvenorOdd(4);//method call
-		System.out.println("result:"+result);
+		//String result=ctrStatements.verifyEvenorOdd(4);//method call
+		//System.out.println("result:"+result);
 		
 		//ctrStatements.evenoddascendforloop(15, 20);
 		//ctrStatements.evenoddascendwhileloop(1,10);
-		String feedback = ctrStatements.getFeedback(10);
-		System.out.println(feedback);
+		//String feedback = ctrStatements.getFeedback(10);
+		//System.out.println(feedback);
+		
+		
+		//ctrStatements.verifyPrimeNumber(9);
+	
+		//ctrStatements.printOnetoNprimeNumbers(90,100);
+		ctrStatements.printRightAngleTrileft(10);
 		
 		
 		
@@ -78,6 +84,61 @@ public class HandleCtrStatements {
 		}
      }
      
+     
+     private void verifyPrimeNumber(int val)
+     {
+    	 
+    	 int count=0;
+    	 for(int i=1;i<=val;i++) {
+    		 System.out.println(val+" divide with "+i);
+    		 if(val%i==0) {
+    			 count=count+1;
+    			 System.out.println(val+" divisible by "+i);
+    		 }
+    		/* if(i==val) {
+    			 break;
+    		 }*/
+    		 
+    	 }
+    	 if(count==2) {
+    		 System.out.println(val+" is prime number:");
+    	 }
+    	 else {
+    		 System.out.println(val+" is not prime number:");
+    	 }
+    	 
+     }
+     
+     private void printOnetoNprimeNumbers(int startendex,int lastendex)
+     {
+    	 for(int i=startendex;i<=lastendex;i++) {
+    		 int count=0;
+    		 for(int j=1;j<=i;j++) {
+    			 if(i%j==0) {
+        			 count=count+1;
+        			// System.out.println(i+" divisible by "+j);
+        		 }
+    		 }
+    		 if(count==2) {
+        		 System.out.println(i+" is prime number:");
+        	 }
+        	 else {
+        		 //System.out.println(i+" is not prime number:");
+        	 }
+    	 }
+    	 
+     }
+     
+     private void printRightAngleTrileft(int lastrow) {
+    	 for(int row=1;row<=lastrow;row++) {
+    		 for(int star=1;star<=row;star++)
+    		 {
+    			 
+    			 System.out.print("*");
+    		 }
+    		 System.out.println("");
+    	 }
+     }
      
      
      
